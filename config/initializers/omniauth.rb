b9d07f78305808b78af590442e69cb20f7bbd0ed
amcaplan <ariel.caplan@mail.yu.edu>
2014-03-18 22:50:57 -0400

    Consign Facebook App Secret to ENV (after resetting online)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1433301626912409', '9c02cab85e4ecf966af7486cada6660d'
+  provider :facebook, '1433301626912409', ENV['FACEBOOK_APP_SECRET']
 end
