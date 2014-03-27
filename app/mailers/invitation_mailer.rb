a15fa2ae78228023860c33d07a0e4bf53fd83749
amcaplan <ariel.caplan@mail.yu.edu>
2014-03-27 12:53:27 -0400

    Write up starter HTML email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,10 +1,12 @@
 class InvitationMailer < ActionMailer::Base
   default from: "The Eateract Team <eateract@gmail.com>"
 
-  def invited_email(person)
-    @person = person
+  def invited_email(meal_person)
+    @meal_person = meal_person
+    @person = meal_person.person
+    @meal = meal_person.meal
     attachments.inline['logo.png'] = File.read(File.expand_path('app/assets/images/logo_email.png'))
     mail(to: "#{@person.name} <#{@person.email}>",
-      subject: "You're Invited to a Meal!",) 
+      subject: "You're Invited to a Meal!") 
   end
 end
