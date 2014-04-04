9a0a6f1f04c37adb55138ed3f9bc280f54643ca7
amcaplan <ariel.caplan@mail.yu.edu>
2014-04-04 13:08:36 -0400

    Add host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,19 @@ Eateract::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'eateract@gmail.com',
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
+  config.action_mailer.default_url_options = { host: 'eateract.herokuapp.com' }
 end
